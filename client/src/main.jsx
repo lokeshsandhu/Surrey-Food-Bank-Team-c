0f6390e3092d09ba6ef8639007153f0a80e0bf54
aoik109 <allison.ko.family@gmail.com>
2026-02-18 13:00:05 -0500

    finished setting up mantine in client and packages/auth

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import './index.css'
 import '@mantine/core/styles.css';
 import '@mantine/nprogress/styles.css'
 
-import { MantineProvider } from '@mantine/core';
+import './index.css'
+
+import { createTheme, MantineProvider } from '@mantine/core';
 
 import App from './App.jsx'
 
+const theme = createTheme({});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MantineProvider>
+    <MantineProvider theme={theme}>
       <App />
     </MantineProvider>
-  </StrictMode>,
+  </StrictMode>
 )
--- packages/auth/src/main.jsx
+++ packages/auth/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import { createTheme, MantineProvider } from '@mantine/core'
+
+import '@mantine/core/styles.css';
+import '@mantine/nprogress/styles.css'
+
 import './index.css'
+
 import App from './Login.jsx'
+
+const theme = createTheme({})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
+  </StrictMode>
 )
