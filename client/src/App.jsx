64d3e413f2666996e505f56d4053fdcc2008b517
BlightedHero <0428sanyu@gmail.com>
2026-02-20 11:36:58 -0800

    quick fix to client app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import '@mantine/core/styles.css';
 import '@mantine/notifications/styles.css'
