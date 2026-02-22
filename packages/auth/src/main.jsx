1e4809f81f6b9745ce640b61dacc1ce6b6e4d8f4
johnl <johnlman@student.ubc.ca>
2026-02-22 10:12:34 -0800

    added react-router-dom dependency

--- packages/auth/src/main.jsx
+++ packages/auth/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import { createTheme, MantineProvider } from '@mantine/core'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css'
@@ -16,8 +17,13 @@ const theme = createTheme({})
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <MantineProvider theme={theme}>
-      <LoginPage />
-      <RegisterPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      </BrowserRouter>
     </MantineProvider>
   </StrictMode>
 )
